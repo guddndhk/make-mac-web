0877b33947eeb1d06a61b1124ef8910ef2df46cf
guddndhk <guddndhk@naver.com>
2021-10-12 04:57:27 +0900

    Test,lombok ,domain(Posts) 생성완료 내일 jpa 작업 들어갈예정

---
+++ src/test/java/com/mac/book/springboot/HelloControllerTest.java
@@ -0,0 +1,45 @@
+package com.mac.book.springboot;
+import com.mac.book.springboot.web.HelloController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.hamcrest.Matchers.is;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    //hello 리턴 테스트
+    @Test
+    public void hello() throws Exception{
+        String hello = "hello";
+
+        mvc.perform(get("/hello")).andExpect(status().isOk()).andExpect(content().string(hello));
+    }
+
+    //helloDto 리턴 테스트
+    @Test
+    public void helloDtoReturn() throws Exception{
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto").param("name",name)
+                        .param("amount", String.valueOf(amount)))
+                        .andExpect(status().isOk())
+                        .andExpect(jsonPath("$.name",is(name)))
+                        .andExpect(jsonPath("$.amount",is(amount)));
+    }
+
+
+}
