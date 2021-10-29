0f33cfa4da4ae71d64a7c63ac02e59cc8a3db2c7
guddndhk <guddndhk@naver.com>
2021-10-30 03:57:18 +0900

    2021/10/30 컴밋 테스트

--- src/main/java/com/mac/book/springboot/config/JpaConfig.java
+++ src/main/java/com/mac/book/springboot/config/JpaConfig.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
-@EnableJpaAuditing//Application class 에서 해당 클래스로 이동해옴 단위 테스트로 인해..
+@EnableJpaAuditing//Application class 에서 해당 클래스로 이동해옴 단위 테스트로 인해....
 public class JpaConfig {
 }
